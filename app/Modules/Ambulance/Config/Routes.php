1908231c1d93ae11ea7994d1639a72e12e654b5d
Nehemia <nehemia.obati@kingsway.co.ke>
2026-06-04 18:18:40 +0300

    chore: remove redundant project documentation and update ambulance module routes

--- app/Modules/Ambulance/Config/Routes.php
+++ app/Modules/Ambulance/Config/Routes.php
@@ -11,7 +11,7 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->group('ambulance', ['namespace' => 'App\Modules\Ambulance\Controllers', 'filter' => ['auth', 'role:paramedic']], static function ($routes) {
     $routes->get('/', 'AmbulanceController::home', ['as' => 'ambulance.home']);
-    $routes->get('hospital/(:num)', 'AmbulanceController::detail/$1', ['as' => 'ambulance.hospital.detail']);
+    $routes->get('hospital/(:segment)', 'AmbulanceController::detail/$1', ['as' => 'ambulance.hospital.detail']);
     $routes->get('pre-notify/(:num)', 'AmbulanceController::preNotifyForm/$1', ['as' => 'ambulance.pre_notify']);
     $routes->post('pre-notify', 'AmbulanceController::sendPreNotification', ['as' => 'ambulance.pre_notify.submit']);
     $routes->get('run/(:num)', 'AmbulanceController::activeRun/$1', ['as' => 'ambulance.active_run']);
